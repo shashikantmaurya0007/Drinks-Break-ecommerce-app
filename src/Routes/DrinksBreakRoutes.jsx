8a5068606ed72ab643e1163a57afbf1c0b3bd201
shashikantmaurya0007 <80745766+shashikantmaurya0007@users.noreply.github.com>
2022-03-22 02:59:27 +0530

    feat:completed Router

--- src/Routes/DrinksBreakRoutes.jsx
+++ src/Routes/DrinksBreakRoutes.jsx
@@ -5,15 +5,19 @@ import {
   Cart,
   WishList,
   ProductsListing,
+  Login,
+  Signup,
 } from "../pages/index.js";
 const DrinksBreakRoutes = () => {
   return (
     <div className="routes_height">
       <Routes>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/wishlist" element={<WishList />}></Route>
-        <Route path="/products" element={<ProductsListing />}></Route>
-        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<WishList />} />
+        <Route path="/products" element={<ProductsListing />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </div>
   );
