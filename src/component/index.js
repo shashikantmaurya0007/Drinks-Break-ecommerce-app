d217bb2ae385acafd8a2419277a9e5de08edd3f7
shashikantmaurya0007 <80745766+shashikantmaurya0007@users.noreply.github.com>
2022-04-07 23:48:38 +0530

    refactor:e-com

--- src/component/index.js
+++ src/component/index.js
@@ -1,4 +1,6 @@
 import { Filter } from "./Filter";
 import { ProductCard } from "./ProductCard";
 import { FilteredProducts } from "./FilteredProducts";
+export { Error }
+from "./Error";
 export { Filter, ProductCard, FilteredProducts };
