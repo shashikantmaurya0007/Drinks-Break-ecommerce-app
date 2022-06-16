094210623cdad8fb3803b9d0fc0a4f97ae3ca39e
shashikant maurya <shashikantmaurya0007@gmail.com>
2022-06-17 03:55:36 +0530

    feat:search-feature-completed

--- src/PortalComponent/SearchPortal.jsx
+++ src/PortalComponent/SearchPortal.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { ProductCard } from "../component/ProductCard";
 import { useDebounce } from "../customHooks/useDebounce";
-import { FILTER_ACTION, useProducts } from "../state";
+import { FILTER_ACTION, useProducts, useSearch } from "../state";
 import { useFilter } from "../state";
 import { filterinitialstate } from "../state";
 import "./SearchPortal.css";
@@ -16,10 +17,17 @@ const SearchPortal = () => {
   } = useProducts();
 
   const inputRef = useRef();
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-
+  const { setShowSearchModal } = useSearch();
+  const closeSearchBar = () => {
+    setShowSearchModal((prev) => false);
+    filterDispatch({
+      type: FILTER_ACTION.SORT_BY_SEARCH,
+      payload: {
+        ...filterinitialstate,
+        searchText: "",
+      },
+    });
+  };
   const search = (e) => {
     filterDispatch({
       type: FILTER_ACTION.SORT_BY_SEARCH,
@@ -32,25 +40,60 @@ const SearchPortal = () => {
 
   const searchResultProucts = products?.filter(
     (prod) =>
-      prod.brand.toLowerCase().includes(searchText.toLowerCase()) ||
-      prod.category.toLowerCase()
+      prod.brand
+        .toLowerCase()
+        .trim()
+        .includes(searchText.toLowerCase().trim()) ||
+      prod.category.toLowerCase().trim().includes(searchText.toLowerCase()) ||
+      prod.title.toLowerCase().trim().includes(searchText.toLowerCase())
   );
 
   const debounceSearch = useDebounce(search, 400);
+  const noProductFound = !Boolean(searchResultProucts?.length);
 
+  useEffect(() => {
+    inputRef.current.focus();
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
   return ReactDOM.createPortal(
     <div className="outer_search_con">
-      <div className="search_con">
-        <div>
-          <input
-            ref={inputRef}
-            placeholder="Search your drink here...."
-            className="searchPortalSearch"
-            type="text"
-            onChange={(e) => debounceSearch(e)}
-          />
+      <div className=" search_content">
+        <div className="search_con">
+          <div>
+            <input
+              ref={inputRef}
+              placeholder="Search your drink here...."
+              className="searchPortalSearch"
+              type="text"
+              onChange={(e) => debounceSearch(e)}
+            />
+          </div>
         </div>
+        {!noProductFound && (
+          <div className="search_product_list">
+            {searchResultProucts?.map((prod) => (
+              <ProductCard product={prod} />
+            ))}
+          </div>
+        )}
+        {noProductFound && (
+          <div className="no_result_found">
+            <p> No Product found for "{searchText}"</p>
+            <img
+              className="img_no_result_found"
+              src="/assets/NoSearchResult.png"
+              alt=""
+            />
+          </div>
+        )}
       </div>
+
+      <button onClick={() => closeSearchBar()} className="search_cancel_btn">
+        <i class="bi bi-x-square-fill"></i>
+      </button>
     </div>,
     document.getElementById("searchPortal")
   );
